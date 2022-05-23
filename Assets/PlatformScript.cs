500add818f67add62749cafed99d30e0c74e4324
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-05-23 12:28:59 -0300

    Vectror 3 Section exercises done

--- Assets/PlatformScript.cs
+++ Assets/PlatformScript.cs
@@ -9,10 +9,17 @@ public class PlatformScript : MonoBehaviour
     public float xLeftLimit;
     public bool toRight;
 
+    public GameObject prefab1;
+    public GameObject prefab2;
+
+    public GameObject baseDer;
+    public GameObject baseIzq;
+
     // Start is called before the first frame update
     void Start()
     {
         toRight = true;
+
     }
 
     // Update is called once per frame
@@ -30,9 +37,16 @@ public class PlatformScript : MonoBehaviour
         if (transform.position.x >= xRightLimit)
         {
             toRight = false;
+            GameObject clon;
+            clon = Instantiate(prefab1);
+            clon.transform.position = baseDer.transform.position + new Vector3(0,1,0);
+
         }else if(transform.position.x <= xLeftLimit)
         {
             toRight = true;
+            GameObject clon;
+            clon = Instantiate(prefab2);
+            clon.transform.position = baseIzq.transform.position + new Vector3(0, 1, 0);
         }
     }
 }
